ba0a1c444c2a2fd4b588af7af5d02daa0f1a28dd
a.kulmakhanov <a.kulmakhanov@forusdata.com>
2023-08-30 11:46:33 +0600

    fixes in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -22,35 +22,4 @@ export default {
 
 <style scoped>
 
-
-.marquee-container {
-  width: 100%;
-  position: relative;
-  overflow: hidden;
-  white-space: nowrap;
-}
-.marquee-content {
-  display: inline-block;
-  animation: marquee 30s linear infinite;
-}
-.marquee-item {
-  display: inline-flex;
-  align-items: center;
-  margin-right: 10px;
-}
-.marquee-text {
-  margin: 0;
-}
-.marquee-img {
-  height: auto;
-  margin-right: 10px;
-}
-@keyframes marquee {
-  0% {
-    transform: translateX(100%);
-  }
-  100% {
-    transform: translateX(-100%);
-  }
-}
 </style>
